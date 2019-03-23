47a292834dd27a31ee6f1759d69a2ace97518a68
Hari Krishna Majety <hmajety@ncsu.edu>
2019-03-22 20:53:51 -0400

    Docker setup script

---
+++ dockerSetup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install  curl  apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+sudo docker pull ubuntu
