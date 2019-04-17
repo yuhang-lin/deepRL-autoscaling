969eb6c6c5a834678a3b18c46a16a417ec5fc546
Srinivas Parasa <sparasa@ncsu.edu>
2019-04-16 20:02:14 -0400

    modularised getcpuinfo function for easy integration

--- infra/getcpuinfo.py
+++ infra/getcpuinfo.py
@@ -3,47 +3,52 @@ import requests
 import json
 import time
 
-headers = {
-    'Content-type': 'application/json',
-}
+def cpuinfo():
 
-
-#response=requests.get('http://localhost:8080/api/v1.3/containers/docker', headers=headers)
-
-#response=json.loads(response.content)
-
-conlist=[]
-
-it=0
-conlist.append('rubis-wg-0')
-conlist.append('rubis-control-tier-0')
-conlist.append('vmtouch-rubis-db-data-0')
-conlist.append('rubis-web-tier-0')
-conlist.append('rubis-db-tier-0')
+	headers = {
+    	'Content-type': 'application/json',
+	}
 
 
 
-# for each container get the current cpu time
-it=0
+	conlist=[]
 
-while it<len(conlist):
-        #http://152.46.18.217:8080/api/v2.0/stats/rubis_rubis_1?type=docker&count=1
-        req='http://152.46.18.217:8080/api/v2.0/stats/'+str(conlist[it])+'?type=docker&count=2'
-        response = requests.get(req, headers=headers)
+	it=0
+	conlist.append('rubis-wg-0')
+	conlist.append('rubis-control-tier-0')
+	conlist.append('vmtouch-rubis-db-data-0')
+	conlist.append('rubis-web-tier-0')
+	conlist.append('rubis-db-tier-0')
 
-        coninfo=json.loads(response.content)
 
-        #get cpu info now
-        contid=str(conlist[it])
 
-        #get keys in this dictionary
-        dickeys=coninfo.keys()
+	# for each container get the current cpu time
+	it=0
 
-        cpu_percentage=coninfo[dickeys[0]][1]['cpu_inst']['usage']['total']/(10**7)
+	cpudata=[]
 
-        print(cpu_percentage) 
+	while it<len(conlist):
+        	req='http://152.46.18.217:8080/api/v2.0/stats/'+str(conlist[it])+'?type=docker&count=2'
+        	response = requests.get(req, headers=headers)
+
+        	coninfo=json.loads(response.content)
+
+        	#get cpu info now
+        	contid=str(conlist[it])
+
+        	#get keys in this dictionary
+        	dickeys=coninfo.keys()
+
+        	cpu_percentage=coninfo[dickeys[0]][1]['cpu_inst']['usage']['total']/(10**7)
+
+        	cpudata.append(cpu_percentage)
           
-        it=it+1
-          
-	#time.sleep(10)
-print("completed")
+        	it=it+1
+	return cpudata
+
+
+def main():
+  print("Hello world")
+  
+if __name__== "__main__":
+  main()
