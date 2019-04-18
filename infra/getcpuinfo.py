d7ba2c73a16762ef47ecf0e5a87d895995c23269
Srinivas Parasa <sparasa@ncsu>
2019-04-18 13:26:15 -0400

    made code compatible with 3.0

--- infra/getcpuinfo.py
+++ infra/getcpuinfo.py
@@ -5,51 +5,52 @@ import time
 
 def cpuinfo():
 
-	headers = {
-    	'Content-type': 'application/json',
-	}
+    headers = { 
+        'Content-type': 'application/json',
+    }   
 
 
 
-	conlist=[]
+    conlist=[]
 
-	it=0
-	conlist.append('rubis-wg-0')
-	conlist.append('rubis-control-tier-0')
-	conlist.append('vmtouch-rubis-db-data-0')
-	conlist.append('rubis-web-tier-0')
-	conlist.append('rubis-db-tier-0')
+    it=0
+    conlist.append('rubis-wg-0')
+    conlist.append('rubis-control-tier-0')
+    conlist.append('vmtouch-rubis-db-data-0')
+    conlist.append('rubis-web-tier-0')
+    conlist.append('rubis-db-tier-0')
 
 
 
-	# for each container get the current cpu time
-	it=0
+    # for each container get the current cpu time
+    it=0
 
-	cpudata=[]
+    cpudata=[]
 
-	while it<len(conlist):
-        	req='http://152.46.19.96:8080/api/v2.0/stats/'+str(conlist[it])+'?type=docker&count=2'
-        	response = requests.get(req, headers=headers)
+    while it<len(conlist):
+        req='http://152.46.19.96:8080/api/v2.0/stats/'+str(conlist[it])+'?type=docker&count=2'
 
-        	coninfo=json.loads(response.content)
+        response = requests.get(req, headers=headers)
 
-        	#get cpu info now
-        	contid=str(conlist[it])
+        coninfo=json.loads(response.content.decode('utf-8'))
 
-        	#get keys in this dictionary
-        	dickeys=coninfo.keys()
+        #get cpu info now
+        contid=str(conlist[it])
 
-        	cpu_percentage=coninfo[dickeys[0]][1]['cpu_inst']['usage']['total']/(10**7)
+        #get keys in this dictionary
+        dickeys=list(coninfo.keys())
 
-        	cpudata.append(cpu_percentage)
-          
-        	it=it+1
-	return cpudata
+        cpu_percentage=coninfo[dickeys[0]][1]['cpu_inst']['usage']['total']/(10**7)
+
+        cpudata.append(cpu_percentage)
+
+        it=it+1
+    return cpudata
 
 
 def main():
-	print("Hello world")
-	cpuinfo()
-  
+    print("Hello world")
+    cpuinfo()
+
 if __name__== "__main__":
   main()
